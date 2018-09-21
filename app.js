23116c2bdbc776f3482d9082dfd4431ec80b2cda
mac <samer.mnr@gmail.com>
2018-09-21 15:53:50 +0200

    add calculate

--- app.js
+++ app.js
@@ -26,3 +26,35 @@ window.onload = function () {
     
     setInterval(showTime, 500);
 };
+
+document.querySelector('#form').addEventListener('submit', function (e) {
+    e.preventDefault();
+})
+
+var inputOne = document.getElementById("input1");
+var inputTwo = document.getElementById("input2");
+var buton1 = document.getElementById('button1'),
+    buton2 = document.getElementById('button2'),
+    buton3 = document.getElementById('button3'),
+    buton4 = document.getElementById('button4'),
+    equal1 = document.getElementById('equal');
+    buton1.addEventListener('click', function () {
+        console.log(inputOne, inputTwo);
+        
+        equal1.innerHTML = Number(inputOne.value) + Number(inputTwo.value);
+    });
+    buton2.addEventListener('click', function () {
+        console.log(inputOne, inputTwo);
+        
+        equal1.innerHTML = inputOne.value - inputTwo.value;
+    });
+    buton3.addEventListener('click', function () {
+        console.log(inputOne, inputTwo);
+        
+        equal1.innerHTML = inputOne.value * inputTwo.value;
+    });
+    buton4.addEventListener('click', function () {
+        console.log(inputOne, inputTwo);
+        
+        equal1.innerHTML = inputOne.value / inputTwo.value;
+    });
